17c614a582278cf368ff5f62600dcb267272d733
mohamedmehdigara <garamehdi57@yahoo.com>
2024-01-23 15:07:59 +0100

    edited files

--- src/components/Asteroid.js
+++ src/components/Asteroid.js
@@ -1,14 +1,40 @@
 // Asteroid.js
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+// Keyframe animation for asteroid rotation
+const rotateAnimation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+// Keyframe animation for asteroid floating
+const floatAnimation = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-10px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
 
 const AsteroidContainer = styled.div`
- 
+  width: ${({ size }) => `${size}px`};
+  height: ${({ size }) => `${size}px`};
   background-color: gray;
   border-radius: 50%;
   position: absolute;
   left: ${({ left }) => `${left}%`};
   top: ${({ top }) => `${top}%`};
+  animation: ${rotateAnimation} 10s linear infinite, ${floatAnimation} 5s ease-in-out infinite;
+  // Add more styles to customize the appearance of the asteroid
 `;
 
 const Asteroid = ({ size, left, top }) => {
